b0de1f0273d65abd48a90fd592a5ab328893821b
deletree_yang <76205590+deletree@users.noreply.github.com>
2020-12-24 16:02:10 +0800

    Update config_flow.py

--- RS-485-Gateway/custom_components/rs-485_gateway1/config_flow.py
+++ RS-485-Gateway/custom_components/rs-485_gateway1/config_flow.py
@@ -7,16 +7,13 @@ from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from . import DOMAIN
-from .core import gateway1
+#from .core import gateway1
 
 _LOGGER = logging.getLogger(__name__)
 
-ACTIONS = {
-    'token': "Add Gateway using Token"
-}
-
-SERVERS = {
-    'cn': "China",
+PORTS = {
+    'port1': "Add Port1 device type.",
+    'port2': "Add Port2 device type."
 }
 
 
@@ -27,7 +24,7 @@ class RS485Gateway1FlowHandler(ConfigFlow, domain=DOMAIN):
            return self.async_show_form(
                     step_id='port2',
                     data_schema=vol.Schema({
-                        vol.Required('port2', default='tty/01': str
+                        vol.Required('port2', default='tty/01': vol.In(ACTIONS)
                     }),
                     description_placeholders={'error_text': ''}
                 )
@@ -35,7 +32,7 @@ class RS485Gateway1FlowHandler(ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id='port1',
             data_schema=vol.Schema({
-                vol.Required('action', default='cloud'): vol.In(ACTIONS)
+                vol.Required('port1', default='tty/usb'): vol.In(ACTIONS)
             })
         )
 
